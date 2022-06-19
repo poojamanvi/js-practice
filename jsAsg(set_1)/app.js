82baff582486d5a38747d04fa7d6f97f1a265e39
Pooja <poojadahiya121001@gmail.com>
2022-06-19 16:34:03 +0530

    add

--- jsAsg(set_1)/app.js
+++ jsAsg(set_1)/app.js
@@ -10,3 +10,34 @@ for(let i=1;i<=25;i++){
 }
 
 }
+// Question no. 2
+// Print the sum of all even numbers in a range from a to b. (Including a,b)
+let sum = 0;
+for( let i=1; i<=30; i++){
+    if(i%2==0){
+         sum+=i
+    }
+   
+}
+ console.log(sum);
+
+
+// Question No. 3
+// Write a program to read the first and last digit of a number
+
+let n,firstDigit,lastDigit;
+n = 12345789410
+lastDigit = (n%10)
+
+console.log("last Digit",
+    lastDigit
+)
+
+
+while(n>=10){
+    firstDigit=Math.floor(n/10)
+    n/=10;
+    
+}
+console.log("first digit",firstDigit)
+
--- jsAsg(set_1)/readme.md
+++ jsAsg(set_1)/readme.md
@@ -1,2 +1,7 @@
 <h1>Output<h1>
-<img src="jsAsg(set_1)/image/question-1.PNG" alt="img">
+<h2>Question -1<h2>
+<img src="image/Question-1.PNG" alt="img">
+<h2>Question -2<h2>
+<img src="image/Question-2.PNG" alt="img">
+<h2>Question -3<h2>
+<img src="image/Question-3.PNG" alt="img">
