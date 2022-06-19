fac2fff315c3af28748bdef11d2ffc16d67952c1
Pooja <poojadahiya121001@gmail.com>
2022-06-19 10:16:38 +0530

    first question

---
+++ jsAsg(set_1)/app.js
@@ -0,0 +1,12 @@
+// assignment question
+// Question no - 1
+
+// Using a loop print all odd numbers up to and including n. Don’t include 0.
+
+
+for(let i=1;i<=25;i++){
+    if(i%2!=0){
+    console.log(i)
+}
+
+}
---
+++ jsAsg(set_1)/readme.md
@@ -0,0 +1,2 @@
+<<h1>>Output<h1>
+<img src="question-1.PNG">
