88d6a6995e805315f2c3a2665015c267627ee8f0
Pooja <poojadahiya121001@gmail.com>
2022-07-05 22:15:10 +0530

    add

--- array.js
+++ array.js
@@ -3,10 +3,66 @@
 
 // console.log(num,copy)
 
-let num = [10,21]
-let copy = num
+// let num = [10,21]
+// let copy = num
 
-console.log(num,copy)
-copy[0] = 15
-num[1] = 30
-console.log(num,copy)
+// console.log(num,copy)
+// copy[0] = 15
+// num[1] = 30
+// console.log(num,copy)
+
+// // 
+// let num = 10;
+// console.log(num);
+
+
+// num = 'pooja'
+
+// console.log(num);
+
+// let rajaniFamily = {
+//     name : 'Rajni',
+//     age : 75,
+//     daughter :{
+//         name :  'Ishwarya',
+//         age : 40,
+//     }
+// }
+
+// // let names = ['p','q','f','g']
+// let names = ['p',10,true,{
+//     name :  'Ishwarya',
+//     age : 40,
+// }]
+
+// // console.log(rajaniFamily.daughter.name,rajaniFamily.name,rajaniFamily.age)
+// // console.log(typeof rajaniFamily);
+// // console.log(typeof rajaniFamily.name);
+// // console.log(typeof rajaniFamily.daughter.name);
+// // console.log(rajaniFamily['daughter']['age']);
+// console.log(names);
+
+// let names = ['asc','cbv','dhc']
+
+// names[10] = 'vasu'
+// console.log(names);
+// names[2] = 123
+// console.log(names);
+
+let nums = ['a','b','c','d']
+
+console.log('initial',nums);
+console.log(nums.push('ab'));
+console.log('adding',nums);
+console.log('passing 2',nums.pop(2));
+console.log('popping',nums);
+console.log(nums.pop());
+console.log('popping',nums);
+nums.push('pooja')
+console.log(nums);
+nums.pop()
+nums.pop()
+nums.pop()
+nums.pop()
+console.log(nums);
+
