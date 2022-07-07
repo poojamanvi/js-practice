19437e2d218e04cc621bd84af7374752d4291165
Pooja <poojadahiya121001@gmail.com>
2022-07-07 22:00:21 +0530

    add

--- array.js
+++ array.js
@@ -49,20 +49,56 @@
 // names[2] = 123
 // console.log(names);
 
-let nums = ['a','b','c','d']
+// let nums = ['a','b','c','d']
 
-console.log('initial',nums);
-console.log(nums.push('ab'));
-console.log('adding',nums);
-console.log('passing 2',nums.pop(2));
-console.log('popping',nums);
-console.log(nums.pop());
-console.log('popping',nums);
-nums.push('pooja')
-console.log(nums);
-nums.pop()
-nums.pop()
-nums.pop()
-nums.pop()
-console.log(nums);
+// console.log('initial',nums);
+// console.log(nums.push('ab'));
+// console.log('adding',nums);
+// console.log('passing 2',nums.pop(2));
+// console.log('popping',nums);
+// console.log(nums.pop());
+// console.log('popping',nums);
+// nums.push('pooja')
+// console.log(nums);
+// nums.pop()
+// nums.pop()
+// nums.pop()
+// nums.pop()
+// console.log(nums);
+
+// let nums = ['bike','Scooter','Car','Truck','JCB','Helicopter','Truck','JCB']
+// console.log(nums.slice(2,5))
+// console.log(nums.slice(5,2))
+// console.log(nums.slice(4,7))
+// console.log(nums.slice(5))
+
+//   nums.splice(4,0,'abc');
+//   console.log(nums)
+
+//   console.log(nums[0],nums[1])
+
+// let slicedNums=nums.slice(0,2);
+
+// console.log(nums, slicedNums)
+
+// console.log(nums.slice(0,-3))
+// console.log(nums.slice(-4,4))
+// console.log(nums.slice(4))
+// console.log(nums.slice(-4))
+// console.log(nums.slice(-10))
+
+
+let leftnumbers = [1,2,3,4,5,1,2,5,7,8]
+let rightHand = []
+
+for(let i=0;i<leftnumbers;i++){
+    rightHand.includes(leftnumbers[i])
+}
+
+
+
+
+
+
+
 
